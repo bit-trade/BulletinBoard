70456d92ddc4aef155286dd8166b2d7ae9a82baa
bit-trade <is@uain.su>
2024-10-10 11:48:36 +0300

    config work space

--- site/bb_announcement/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class BbAnnouncementConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'bb_announcement'
--- site/bulletin_board/secret.py
+++
@@ -1,6 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-
-load_dotenv()
-django_key = os.environ.get('SECRET_KEY')
